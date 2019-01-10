5bd6af04141487936c252882b27751138a400945
木下晃兵 <kinoshitakohei@MacBook-Air-2.local>
2019-01-10 16:42:02 +0900

    [Finish]2-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'fc356b9e80d204a062532b3e134a4a695a04f8063c2e2f9ffed794074d26e6a9041ee41fd06bf2a8058511a5c7aa50e45d545fe6c22a85afa8674935fdfe46cb'
